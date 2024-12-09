e864eaa3599a43489b07134a8d99fdf8f2530cde
javoxhr <javohiruzum@gmail.com>
2024-12-09 16:34:00 +0500

    added new components

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ const menuOpen = document.querySelector(".menu-open")
 const menuOverlay = document.querySelector(".overlay")
 
 
+function removeProfil() {
+    menuOpen.classList.remove('menu-open-active')
+    menuOverlay.classList.remove('menu-overlay-active')
+}
+
 openMenu.addEventListener('click', ()=> {
     menuOpen.classList.add('menu-open-active')
     menuOverlay.classList.add('menu-overlay-active')
 })
 
 menuOverlay.addEventListener('click', ()=> {
-    menuOpen.classList.remove('menu-open-active')
-    menuOverlay.classList.remove('menu-overlay-active')
+    removeProfil()
+})
+
+const settings = document.querySelector('.settings')
+const settingsBtn = document.querySelector('#settings-btn')
+
+settingsBtn.addEventListener('click', ()=> {
+    removeProfil()
+    settings.classList.add('active-settings')
+})
+
+const backInSettings = document.querySelector('#back-in-settings')
+
+
+backInSettings.addEventListener('click', ()=> {
+    settings.classList.remove('active-settings')
+    menuOpen.classList.add('menu-open-active')
+    menuOverlay.classList.add('menu-overlay-active')
 })
