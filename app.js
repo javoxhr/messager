f3ba2ac6639ac2f3c67497ab2e09ce38d7ebb703
javoxhr <javohiruzum@gmail.com>
2024-12-20 18:09:33 +0500

    new

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function removeProfil() {
     menuOverlay.classList.remove('menu-overlay-active')
 }
 
+// function openMenu() {
+//     menuOpen.classList.add('menu-open-active')
+//     menuOverlay.classList.add('menu-overlay-active')
+// }
+
 openMenu.addEventListener('click', ()=> {
     menuOpen.classList.add('menu-open-active')
     menuOverlay.classList.add('menu-overlay-active')
@@ -34,4 +39,33 @@ backInSettings.forEach((el)=> {
         menuOpen.classList.add('menu-open-active')
         menuOverlay.classList.add('menu-overlay-active')
     })
+})
+
+const triTochIns = document.querySelector('.open-tri-toch')
+const triTTochBtn = document.querySelector('#open-tri-toch-btn')
+const triTochOverlay = document.querySelector('.tri-toch-overlay')
+
+triTTochBtn.addEventListener('click', ()=> {
+    triTochIns.classList.toggle('tri-toch-active')
+    triTochOverlay.classList.toggle('tri-toch-active')
+})
+
+triTochOverlay.addEventListener('click', ()=> {
+    triTochIns.classList.toggle('tri-toch-active')
+    triTochOverlay.classList.toggle('tri-toch-active')
+})
+
+const myPrf = document.querySelector('.my-profil')
+const profilBtn = document.querySelector('#profil-btn')
+const backBtnInProfil = document.querySelector('#back-in-prfolie')
+
+profilBtn.addEventListener('click', ()=> {
+    myPrf.classList.add('active-my-profil')
+    removeProfil()
+})
+
+backBtnInProfil.addEventListener('click', ()=> {
+    myPrf.classList.remove('active-my-profil')
+    menuOpen.classList.add('menu-open-active')
+    menuOverlay.classList.add('menu-overlay-active')
 })
